2719fd8a0684298a755de54df0934bf869d7e0ef
Andronic123 <48093299+andronic1234@users.noreply.github.com>
2022-12-27 20:33:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,14 +8,16 @@ let coords = [];
 app.use(router);
 
 const PORT = process.env.PORT || 3000;
-
-router.get(`/player/:name`, function (req, res) {
+router.get('/', function(req, res) {
+  res.send('Welcome to my RealmEye API')
+});
+router.get(`/player/:name`, function(req, res) {
   const player = req.params.name;
   const website = `https://www.realmeye.com/player/${player}`;
   let result = res;
   PlayerInfo(website, result, coords);
 });
-router.get(`/player/:name/:char/:item?`, async function (req, res) {
+router.get(`/player/:name/:char/:item?`, async function(req, res) {
   const player = req.params.name;
   const char = req.params.char;
   const item = req.params.item.toLowerCase();
@@ -24,7 +26,7 @@ router.get(`/player/:name/:char/:item?`, async function (req, res) {
   await itemImg(coords, website, char, res, item);
 });
 
-router.get(`/guild/:guild`, function (req, res) {
+router.get(`/guild/:guild`, function(req, res) {
   const guild = req.params.guild;
   const website = `https://www.realmeye.com/guild/${guild}`;
   let result = res;
