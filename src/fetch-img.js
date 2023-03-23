7a28033da4df75d165feff2629e355fe59bf456a
Andronic123 <fotopoulos.andrew@gmail.com>
2023-03-23 13:36:50 +0200

    auto fetch sheets.png

--- src/fetch-img.js
+++ src/fetch-img.js
@@ -2,17 +2,32 @@ const fs = require("fs");
 const schedule = require("node-schedule");
 const axios = require("axios");
 
-let imgUrl = "https://www.realmeye.com/s/bb/css/renders.png";
+let ItemImgUrl = "https://www.realmeye.com/s/bb/css/renders.png";
+let CharacterImgUrl = "https://www.realmeye.com/s/fj/img/sheets.png";
 
 module.exports.FetchImg = function FetchImg() {
   schedule.scheduleJob("0 0 * * *", async (date) => {
     await axios
-      .get(imgUrl, {
+      .get(ItemImgUrl, {
         responseType: "arraybuffer",
       })
       .then((response) => {
         console.log(date);
         fs.writeFileSync(__dirname + "/resources/renders.png", response.data, {
+          encoding: "base64",
+        });
+      })
+      .catch((ex) => {
+        console.error(ex);
+      });
+
+    await axios
+      .get(CharacterImgUrl, {
+        responseType: "arraybuffer",
+      })
+      .then((response) => {
+        console.log(date);
+        fs.writeFileSync(__dirname + "/resources/sheets.png", response.data, {
           encoding: "base64",
         });
       })
